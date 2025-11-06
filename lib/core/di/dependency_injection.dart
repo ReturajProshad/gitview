a5675200746fe47c3cb8964347d7ebd5ba751a46
ReturajProshad <returajsumon0808@gmail.com>
2025-11-06 20:19:46 +0600

    feat: Set up dependency injection with GetX

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,39 @@
+// lib/core/dependency_injection.dart
+import 'package:get/get.dart';
+import 'package:gitview/core/network/dio_client.dart';
+import 'package:gitview/data/repositories/github_repository_impl.dart';
+import 'package:gitview/data/services/github_api_service.dart';
+import 'package:gitview/domain/repositories/github_repository.dart';
+import 'package:gitview/domain/usecases/get_repository_details_usecase.dart';
+import 'package:gitview/domain/usecases/get_user_repositories_usecase.dart';
+import 'package:gitview/domain/usecases/get_user_usecase.dart';
+
+class DependencyInjection {
+  static void init() {
+    // Network
+    Get.lazyPut(() => DioClient());
+
+    // Services
+    Get.lazyPut(() => GitHubApiService(Get.find<DioClient>()));
+
+    // Repositories
+    Get.lazyPut<GitHubRepository>(
+      () => GitHubRepositoryImpl(Get.find<GitHubApiService>()),
+    );
+
+    // Use Cases
+    Get.lazyPut(() => GetUserUseCase(Get.find<GitHubRepository>()));
+    Get.lazyPut(() => GetUserRepositoriesUseCase(Get.find<GitHubRepository>()));
+    Get.lazyPut(
+      () => GetRepositoryDetailsUseCase(Get.find<GitHubRepository>()),
+    );
+
+    // Controllers
+    // Get.lazyPut(() => UserInputController(Get.find<GetUserUseCase>()));
+    // Get.lazyPut(() => HomeController(Get.find<GetUserRepositoriesUseCase>()));
+    // Get.lazyPut(
+    //   () =>
+    //       RepositoryDetailsController(Get.find<GetRepositoryDetailsUseCase>()),
+    // );
+  }
+}
